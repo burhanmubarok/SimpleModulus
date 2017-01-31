9bb07a1d5ad26ac3113c1da40fa4917fc2dc4354
SMI-WEBSERVER\burhan.mubarok <burhan.mubarok@sinergy.mediaintegrasi.com>
2017-01-31 18:55:31 +0700

    Add class

--- SimpleModulus/Program.cs
+++ SimpleModulus/Program.cs
@@ -8,8 +8,41 @@ namespace SimpleModulus
 {
     class Program
     {
-        static void Main(string[] args)
+        public Program()
         {
+            PrintFunction();
+        }
+
+        static void Main()
+        {
+            new Program();
+            Console.ReadKey();
+        }
+
+        // print from 1 to 100, if mod 3 print "Buzz" instead a number, if mod 5 print "Buzz", if mod 3 and mod 5 print "FizzBuzz"
+        public void PrintFunction()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var isThree = i % 3 == 0;
+                var isFive = i % 5 == 0;
+                if (isThree && !isFive)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (!isThree && isFive)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (isThree && isFive)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
